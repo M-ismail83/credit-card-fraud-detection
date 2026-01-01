b47e1ac0f6c6eea549ef79e6a71242fbb7d9e2a1
Mahmut İsmail Öztürk <mahmutismail83@gmail.com>
2026-01-01 15:21:12 +0300

    Finished the README.md

--- task_miss_val/main.py
+++ task_miss_val/main.py
@@ -12,7 +12,6 @@ plt.figure(figsize=(14, 6))
 # Define simple colors for the bars
 bar_colors = ['gray', 'blue', 'orange', 'green', 'black']
 
-# --- Plot 1: AUPRC ---
 plt.subplot(1, 2, 1) # 1 row, 2 columns, 1st plot
 plt.bar(dataframe_results['Method'], dataframe_results['AUPRC'], color=bar_colors)
 plt.title('AUPRC Scores')
@@ -20,7 +19,6 @@ plt.ylabel('Score')
 plt.xticks(rotation=45) # Rotate names so they fit
 plt.ylim(0, 1.1) # Set limit slightly above 1
 
-# --- Plot 2: ROC-AUC ---
 plt.subplot(1, 2, 2) # 1 row, 2 columns, 2nd plot
 plt.bar(dataframe_results['Method'], dataframe_results['ROC-AUC'], color=bar_colors)
 plt.title('ROC-AUC Scores')
@@ -29,4 +27,5 @@ plt.xticks(rotation=45)
 plt.ylim(0, 1.1)
 
 plt.tight_layout() # Fixes layout so labels don't get cut off
+plt.savefig("task_miss_val/plots/imputation_results.png")
 plt.show()
